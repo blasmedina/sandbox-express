ca927bd2d25d12ed1902922d4eff878fe066ded8
Blas Medina <r.blas.m.c@gmail.com>
2021-05-09 22:40:31 -0400

    :construction: schema validation implementation

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,22 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
 import { getPagination, getPagingData } from '../helpers/pagination.helper';
-import { UserCreationAttributes } from '../models/user';
+import { UserCreationAttributes, userCreationScheme } from '../models/user';
 import UsersRepository from '../repositories/users.repository';
 
 export class UserController {
-  static async create(req: Request, res: Response) {
+  static async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const payload: UserCreationAttributes = req.body;
+      const payload: UserCreationAttributes = await userCreationScheme.validateAsync(req.body);
       const data = await UsersRepository.create(payload);
       return res.json({ data });
-    } catch (error) {}
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async readAll(req: Request, res: Response) {
+  static async readAll(req: Request, res: Response, next: NextFunction) {
     try {
       const data = await UsersRepository.readAll();
       return res.json({ data });
-    } catch (error) {}
+    } catch (error) {
+      next(error);
+    }
   }
 
   static async readAllWithPagination(req: Request, res: Response, next: NextFunction) {
@@ -36,31 +40,39 @@ export class UserController {
       const data = await UsersRepository.readAllWithPagination(limit, offset, filter);
       const response = getPagingData(data, page, limit);
       return res.json(response);
-    } catch (error) {}
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async readById(req: Request, res: Response) {
+  static async readById(req: Request, res: Response, next: NextFunction) {
     try {
       const { id } = req.params;
       const data = await UsersRepository.readById(id);
       return res.json({ data });
-    } catch (error) {}
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async update(req: Request, res: Response) {
+  static async update(req: Request, res: Response, next: NextFunction) {
     try {
-      const payload: UserCreationAttributes = req.body;
+      const payload: UserCreationAttributes = await userCreationScheme.validateAsync(req.body);
       const { id } = req.params;
       const data = await UsersRepository.update(id, payload);
       return res.json({ data });
-    } catch (error) {}
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async delete(req: Request, res: Response) {
+  static async delete(req: Request, res: Response, next: NextFunction) {
     try {
       const { id } = req.params;
       const data = await UsersRepository.delete(id);
       return res.json({ data });
-    } catch (error) {}
+    } catch (error) {
+      next(error);
+    }
   }
 }
